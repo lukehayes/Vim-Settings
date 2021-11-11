7aef3ab2c56f30e7c5eed1385d21b0a991caf289
lukehayes <lukepurplehayes@gmail.com>
2021-11-11 12:19:10 +0000

    Added tile tilemap for all of my projects.

---
+++ OracleTileSet/OracleTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="OracleTileSet" tilewidth="8" tileheight="8" tilecount="96" columns="8">
+ <image source="../Free/8x8.png" width="64" height="96"/>
+</tileset>
